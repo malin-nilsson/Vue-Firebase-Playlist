2d632b0840c41ee35a28418a86f8386495d69f07
Malin Nilsson <malinnilsson87@gmail.com>
2022-11-11 18:48:38 +0100

    Getting real-time playlist data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import getCollection from "@/composables/getCollection";
+
+const { error, documents } = getCollection("playlists");
+</script>
 
 <template>
   <div class="home">Homepage</div>
+  <div v-if="error" class="error">Could not find any playlists.</div>
+  <div v-if="documents">
+    <div v-for="doc in documents" :key="doc.id">
+      <p>{{ doc.title }}</p>
+    </div>
+  </div>
 </template>
